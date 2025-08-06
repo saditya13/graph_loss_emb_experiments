69105821d3c7c74b2ed61da66725517371f40b8a
saditya13 <110767339+saditya13@users.noreply.github.com>
2025-08-06 12:41:42 +0530

    updated sigma exp

--- eval_scripts/print_c.py
+++ eval_scripts/print_c.py
@@ -1,8 +1,6 @@
 import pandas as pd
 def print_classes():
     val = pd.read_csv('../val.csv')
-    print(val.shape())
-    print(val.value_counts())
-
-
+    print(val.shape)
+    print(len(pd.Series(val['label']).value_counts()))
 print_classes()
